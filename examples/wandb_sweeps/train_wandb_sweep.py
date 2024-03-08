24bbe84a43252d67b71203b6cf5674ba97bf3ed2
Othergreengrasses <ciil.sengupta@gmail.com>
2024-03-08 13:37:32 -0500

    implimenting suggestions

--- examples/wandb_sweeps/train_wandb_sweep.py
+++ examples/wandb_sweeps/train_wandb_sweep.py
@@ -41,8 +41,8 @@ def train_sweep(args: argparse.Namespace) -> None:
     # Trains and logs the best checkpoint.
     best_checkpoint = train.train(trainer, model, datamodule, args.train_from)
     util.log_info(f"Best checkpoint: {best_checkpoint}")
-
-   # cleanup
+   # Explicitly deallocates model and clears the cache, based on the following suggestion:
+   #https://github.com/wandb/wandb/issues/1247#issuecomment-1457737657
     del model
     torch.cuda.empty_cache()
     
