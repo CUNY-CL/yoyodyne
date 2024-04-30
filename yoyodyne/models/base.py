b3cf92ec11bb0af9abda176feeb30a54409c0524
Adamits <ajwieme@gmail.com>
2024-04-30 17:25:47 -0600

    Fixes val_ naming

--- yoyodyne/models/base.py
+++ yoyodyne/models/base.py
@@ -325,7 +325,7 @@ class BaseEncoderDecoder(pl.LightningModule):
             for metric_name in self.eval_metrics
         }
         # Always logs validation loss.
-        metrics.update({"val_loss": avg_val_loss})
+        metrics.update({"loss": avg_val_loss})
         # del validation_step_outputs
         for metric, value in metrics.items():
             self.log("val_" + metric, value, prog_bar=True)
