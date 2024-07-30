2567a1d93edebf0b4dd8744f3676d61b617d0425
Kyle Gorman <kylebgorman@gmail.com>
2024-07-30 00:16:00 -0400

    Separates vocabularies for logging.
    Current logic is such that source and target vocabularies (which are stored solely for logging and debugging purposes) are unioned together if --tied_embeddings. However, the purpose of these logging statements has nothing to do with embedding tying, so we modify them so that they are not combined except when building the actual vocabulary. We also remove the special symbols from the logging since they serve no purpose there.

--- yoyodyne/data/indexes.py
+++ yoyodyne/data/indexes.py
@@ -2,7 +2,7 @@
 
 import os
 import pickle
-from typing import Dict, List, Optional, Set
+from typing import Dict, Iterable, Optional, Set
 
 from .. import defaults, special
 
@@ -17,15 +17,18 @@ class Index:
     For consistency, one is recommended to lexicographically sort the
     vocabularies ahead of time."""
 
-    index2symbol: List[str]
-    symbol2index: Dict[str, int]
+    source_vocabulary: List[str]
+    target_vocabulary: List[str]
+    features_vocabulary = Optional[List[str]]
+    _index2symbol: List[str]
+    _symbol2index: Dict[str, int]
 
     def __init__(
         self,
         *,
-        source_vocabulary: List[str],
-        features_vocabulary: Optional[List[str]] = None,
-        target_vocabulary: Optional[List[str]] = None,
+        source_vocabulary: Iterable[str],
+        features_vocabulary: Optional[Iterable[str]] = None,
+        target_vocabulary: Optional[Iterable[str]] = None,
         tie_embeddings: bool = defaults.TIE_EMBEDDINGS,
     ):
         """Initializes the index.
@@ -38,27 +41,23 @@ class Index:
         """
         super().__init__()
         self.tie_embeddings = tie_embeddings
-        # We store all separate vocabularies for logging purposes.
-        # If embeddings are tied, so are the vocab items.
-        # Then, the source and target vocabularies are the union.
+        # We store vocabularies separately for logging purposes.
+        self.source_vocabulary = sorted(source_vocabulary)
+        self.target_vocabulary = sorted(target_vocabalary)
         if self.tie_embeddings:
-            vocabulary = list(
-                sorted(set(source_vocabulary) | set(target_vocabulary))
+            # Vocabulary is the union of source and target.
+            vocabulary = sorted(
+                frozenset(source_vocabulary) | frozenset(target_vocabulary)
             )
-            self.source_vocabulary = special.SPECIAL + vocabulary
-            self.target_vocabulary = special.SPECIAL + vocabulary
         else:
-            # If not tie_embeddings, then the target vocabulary must come
-            # first so that output predictions correctly index our
-            # vocabulary and embeddiings matrix.
+            # Vocabulary consists of target symbols followed by source symbols.
             vocabulary = sorted(target_vocabulary) + sorted(source_vocabulary)
-            self.source_vocabulary = special.SPECIAL + source_vocabulary
-            self.target_vocabulary = special.SPECIAL + target_vocabulary
-        # FeatureInvariantTransformer assumes that features_vocabulary is at
-        # the end of the list.
-        if features_vocabulary:
-            vocabulary.extend(sorted(features_vocabulary))
-        self.features_vocabulary = features_vocabulary
+        # FeatureInvariantTransformer assumes that features_vocabulary is at the end of the vocabulary.
+        if features_vocabulary is not None:
+            self.features_vocabulary = sorted(features_vocabulary)
+            vocabulary.extend(self.features_vocabulary)
+        else:
+            self.features_vocabulary = None
         # Keeps special.SPECIAL first to maintain overlap with features.
         self._index2symbol = special.SPECIAL + vocabulary
         self._symbol2index = {c: i for i, c in enumerate(self._index2symbol)}
