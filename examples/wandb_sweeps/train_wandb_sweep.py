5a286f6883aa81fbdf4a151d3e94b8fb280efaab
Kyle Gorman <kylebgorman@gmail.com>
2024-09-27 12:31:15 -0400

    Update train_wandb_sweep.py

--- examples/wandb_sweeps/train_wandb_sweep.py
+++ examples/wandb_sweeps/train_wandb_sweep.py
@@ -44,20 +44,16 @@ def main() -> None:
     parser = argparse.ArgumentParser(description=__doc__)
     train.add_argparse_args(parser)
     parser.add_argument(
-        "--sweep_id",
-        required=True,
-        help="ID for the sweep to run the agent in.",
+        "--sweep_id", required=True, help="ID for the sweep.",
     )
     parser.add_argument(
-        "--entity", required=True, help="The entity scope for the project."
+        "--entity", required=True, help="Entity name."
     )
     parser.add_argument(
-        "--project", required=True, help="The project of the sweep."
+        "--project", required=True, help="Project name."
     )
     parser.add_argument(
-        "--count",
-        type=int,
-        help="The max number of runs for this agent",
+        "--count", type=int, help="Number of runs to perform.",
     )
     args = parser.parse_args()
     # Forces log_wandb to True, so that the PTL trainer logs runtime metrics
