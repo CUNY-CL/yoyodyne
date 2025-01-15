8b001f8afda1296321f322ca23e283e7abf720e1
Kyle Gorman <kylebgorman@gmail.com>
2025-01-14 21:35:43 -0500

    Adds test data and code to regenerate it

---
+++ tests/testdata/generate.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+"""Generates strings according to some simple criteria."""
+
+import argparse
+import random
+import string
+
+import numpy
+from scipy.stats import poisson
+
+
+def main(args: argparse.Namespace) -> None:
+    random.seed(args.seed)
+    # The docs say that Scipy uses Numpy's randomizer.
+    numpy.random.seed(args.seed)
+    assert 0 < args.vocabulary <= 26
+    vocabulary = list(string.ascii_lowercase[: args.vocabulary])
+    for length in poisson.rvs(mu=args.length, size=args.size):
+        # Avoids length-zero strings.
+        length = max(length, 1)
+        print("".join(random.choice(vocabulary) for _ in range(length)))
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument(
+        "--vocabulary",
+        type=int,
+        required=True,
+        help="value [1, 26] specifying vocabulary size",
+    )
+    parser.add_argument(
+        "--length",
+        type=float,
+        required=True,
+        help="length parameter for Poisson length distribution",
+    )
+    parser.add_argument("--seed", type=int, required=True, help="random seed")
+    parser.add_argument(
+        "--size",
+        type=int,
+        required=True,
+        help="number of examples to generate",
+    )
+    main(parser.parse_args())
